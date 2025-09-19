03861da86f7447a21e1065b083d9736ec7f0b34b
Alckor <alckordev@gmail.com>
2025-09-19 01:05:07 -0500

    chore: update output file names to use .cjs extension for CommonJS builds

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ export default [
   {
     input: 'src/index.ts',
     output: {
-      file: 'dist/index.js',
+      file: 'dist/index.cjs',
       format: 'cjs',
       sourcemap: true,
       exports: 'named',
